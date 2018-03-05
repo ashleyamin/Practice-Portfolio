8263cfc6e7aa485df0600ca1f0cdaeb567e10685
Ashley Amin <ashley.starks@gmail.com>
2018-03-05 16:12:19 -0500

    finished initial structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import cover from './cover.JPG';
+import cover from './coverphoto.png';
 import './App.css';
 
 class App extends Component {
@@ -7,15 +7,24 @@ class App extends Component {
     return (
       <div className="App">
         <header className="Header" style={{backgroundImage: `url(${cover})`}}>
-          <h1 className="Title">Ashley Amin</h1>
-          <p className="Intro">I'm a front end developer and design strategist grounded in business rigor.</p>
-        </header>
-        <body>
-          <div className="Grid-container">
+          <div className="Intro">
+            <h1 className="Title">Ashley Amin</h1>
+            <p className="Tagline">I'm a Front End Developer and Design Strategist grounded in business rigor. Whether in a team or independently, I'm always in search of creating new ideas and executing them to solve problems that help people.</p>
           </div>
+          <a href="#work" className="Scroll-icon"><i className="fas fa-angle-down fa-3x"></i></a>
+        </header>
+        <body className="Body">
+          <p className="test">stuff will go here</p>
         </body>
-        <footer>
-          <p>hello</p>
+        <footer className="Footer">
+          <div className="Footer">
+            <ul>
+              <li><a href="mailto:ashley.starks@gmail.com">Email</a></li>
+              <li><a href="https://www.linkedin.com/in/ashleyamin/" target="_blank">LinkedIn</a></li>
+              <li><a href="https://medium.com/@ashleyamin" target="_blank">Medium</a></li>
+              <li><a href="https://github.com/ashleyamin" target="_blank">Github</a></li>
+            </ul>
+          </div>
         </footer>
       </div>
     );
