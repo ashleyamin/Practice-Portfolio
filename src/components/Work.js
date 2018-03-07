43742e4597635583b1727c717cfe525b805267a0
Ashley Amin <ashley.starks@gmail.com>
2018-03-07 16:00:25 -0500

    changes to the project and github buttons

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,38 +12,43 @@ class Work extends Component {
           <p className="Latest-description">A selection of carefully crafted concepts from past and current projects</p>
         </div>
         <div className="Project-One">
-          <div className="Images-One" style={{backgroundImage: `url(${stoop_main})`}}>
-            <a className="Project-link" href="http://thestoop.herokuapp.com/" target="_blank">
-            <div className="Project-One-Button">the stoop</div>
-            </a>
+          <div className="Images-One" style={{backgroundImage: `url(${stoop_main})`}}></div>
+          <div className="Overview-one">
+            <div className="Description">
+              The Stoop App is a collaborative tool that assists individuals in identifying
+              and sharing experiences about civictech, government and community provided benefits, products
+              and services in NYC. I built this using Ruby on Rails and a custom set of data utilizing three
+              related table structures.
+            </div>
+            <a className="Learn-more-button" href="http://thestoop.herokuapp.com/" target="_blank">see the project</a>
+            <a className="Github-button" href="https://github.com/ashleyamin/The-Stoop" target="_blank">see the code</a>
           </div>
-          <div className="Overview-one">The Stoop App is a collaborative tool that assists individuals in identifying
-            and sharing experiences about civictech, government and community provided benefits, products
-            and services in NYC. I built this using Ruby on Rails and a custom set of data utilizing three
-            related table structures.</div>
         </div>
         <div className="Project-Two">
-          <div className="Images-Two" style={{backgroundImage: `url(${puppy_main})`}}>
-            <a className="Project-link" href="https://puppyfinderapp.herokuapp.com/" target="_blank">
-            <div className="Project-Two-Button">puppyfinder</div>
-            </a>
-          </div>
-          <div className="Overview-two">Puppies make us all happy.
-            Why not make it easier to find and adopt more puppies?
+          <div className="Images-Two" style={{backgroundImage: `url(${puppy_main})`}}></div>
+          <div className="Overview-two">
+            <div className="Description">
+            Making it easier to find and adopt more puppies!
             A group project in our web development immersive program, the PuppyFinder App was built
             collaboratively using React on the front end, Webpack as the build manager, Node.js and the Express
-            framework on the backend, with postgres as the database.</div>
+            framework on the backend, with postgres as the database.
+            </div>
+            <a className="Learn-more-button" href="https://puppyfinderapp.herokuapp.com/" target="_blank">see the project</a>
+            <a className="Github-button" href="https://github.com/ashleyamin/The-Stoop" target="_blank">see the code</a>
+        </div>
         </div>
         <div className="Project-Three">
-          <div className="Images-Three" style={{backgroundImage: `url(${readysetplan_main})`}}>
-            <a className="Project-link" href="http://readysetplan.herokuapp.com/readysetplan" target="_blank">
-            <div className="Project-Three-Button">ready set plan</div>
-            </a>
-          </div>
-          <div className="Overview-three">A medical emergency can happen at anytime. The Ready Set Plan App enables one to
-            plan ahead for a medical crisis by providing a list of essential documents one
-            should have created and stored for a loved one to access in the event of an emergency.
-          Built using HTML/EJS, Node and Express, SQL, pg-promise, and the Google Login API for registration/authorization</div>
+          <div className="Images-Three" style={{backgroundImage: `url(${readysetplan_main})`}}></div>
+          <div className="Overview-three">
+            <div className="Description">
+              A medical emergency can happen at anytime. The Ready Set Plan App enables one to
+              plan ahead for a medical crisis by providing a list of essential documents one
+              should have created and stored for a loved one to access in the event of an emergency.
+              Built using HTML/EJS, Node and Express, SQL, pg-promise, and the Google Login API for registration/authorization
+            </div>
+            <a className="Learn-more-button" href="http://readysetplan.herokuapp.com/readysetplan" target="_blank">see the project</a>
+            <a className="Github-button" href="https://github.com/ashleyamin/The-Stoop" target="_blank">see the code</a>
+        </div>
         </div>
       </div>
     );
