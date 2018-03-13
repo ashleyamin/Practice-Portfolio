059dcec061fc9712a4109cad7efb8c2dcc47794a
Ashley Amin <ashley.starks@gmail.com>
2018-03-13 13:17:23 -0400

    fixed tagline

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <div className="Header" style={{backgroundImage: `url(${cover})`}}>
         <div className="Intro">
           <h1 className="Title">Ashley Amin</h1>
-          <p className="Tagline">I'm a Front End Developer and Design Strategist grounded in business rigor. Whether in a team or independently, I'm always in search of creating new ideas and executing them to solve problems that help people.</p>
+          <p className="Tagline">I'm a Front End Developer and Design Strategist grounded in business rigor. Whether in a team or independently, I'm always in search of creating new ideas and executing them to solve problems that help others.</p>
         </div>
         <a href="#work" className="Scroll-icon"><i className="fas fa-angle-down fa-3x"></i></a>
       </div>
