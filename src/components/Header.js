7e344a38f5ce2afd64df1e0bf919f548a68b98ef
Ashley Amin <ashley.starks@gmail.com>
2018-03-13 11:56:31 -0400

    confirmed build

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
     return (
       <div className="Header" style={{backgroundImage: `url(${cover})`}}>
         <div className="Intro">
-          <h1 className="Title">Ashley AAmin</h1>
+          <h1 className="Title">Ashley Amin</h1>
           <p className="Tagline">I'm a Front End Developer and Design Strategist grounded in business rigor. Whether in a team or independently, I'm always in search of creating new ideas and executing them to solve problems that help people.</p>
         </div>
         <a href="#work" className="Scroll-icon"><i className="fas fa-angle-down fa-3x"></i></a>
